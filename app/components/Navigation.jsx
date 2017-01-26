ed3a2b97a7457886d1692936df2cab9092a9b580
Alexey Kurdyukov <struan.dirk.29@gmail.com>
2017-01-26 23:44:25 +0300

    Initial commit

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,34 @@
+var React = require('react');
+var { Link, IndexLink } = require('react-router');
+
+var Navigation = () => {
+    return (
+        <div className="top-bar">
+            <div className="top-tar-left">
+                <ul className="menu">
+                    <li className="menu-text">
+                        <span>React timer App</span>
+                    </li>
+
+                    <li>
+                        <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+                    </li>
+
+                    <li>
+                        <Link to="/countdown" activeClassName="active-link">Countdown</Link>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="top-bar-right">
+                <ul className="menu">
+                    <li className="menu-text">
+                        Created by <a href="http://www.mead.io" target="_blank">CowexCortex</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+module.exports = Navigation;
