277c91cd1f8a83fbd59d3b5499edce26ea64d51d
Alexey Kurdyukov <struan.dirk.29@gmail.com>
2017-02-04 14:27:46 +0300

    Countdown has been added

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -6,12 +6,34 @@ var Countdown = React.createClass({
     getInitialState: function(){
         return(
             {
-                secondsCount: 0
+                secondsCount: 0,
+                status: 'stopped'
             }
         )
     },
+    componentDidUpdate: function(prevProps, prevState) {
+        if (this.state.status != prevState.status) {
+            switch(this.state.status) {
+                case 'started':
+                     this.startTimer();
+                     break;
+            }
+        }
+    },
+    startTimer: function() {
+        var object = this;
+        this.timer = setInterval(() => {
+            var newSeconds = object.state.secondsCount - 1;
+            object.setState({
+                secondsCount: newSeconds >= 0 ? newSeconds : 0
+            })
+        }, 1000)
+    },
     setSecondsCount: function(count) {
-        this.setState({secondsCount: count});
+        this.setState({
+            secondsCount: count,
+            status: 'started'
+        });
     },
     render: function() {
         return (
